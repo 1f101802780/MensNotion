9508a8f6ddb5662f5da5cbf8c9c55418903d8a10
Subaru <s1f101802780@iniad.org>
2021-08-31 16:20:48 +0900

    login から top へのリンク作成

--- groomings/urls.py
+++ groomings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'groomings'
 urlpatterns = [
-    path(r'', views.top),
-    path(r'login', views.login)
+    path(r'', views.top, name = 'top'),
+    path(r'login', views.login, name = 'login')
 ]
