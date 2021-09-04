1700401c6c112756971f63ed4cc9ba12429556c5
yamamoto.ryunosuke <ryunosukeyamamoto0@gmail.com>
2021-09-04 23:07:44 +0900

    親html(base.html)の作成

--- groomings/urls.py
+++ groomings/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# app_name = 'groomings'
+app_name = 'groomings'
 urlpatterns = [
     path(r'', views.top, name = 'top'),
     path(r'login', views.login, name = 'login'),
