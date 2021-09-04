480bc7810b5b9cfa398f22d07b1f8207720f6a10
yamamoto.ryunosuke <ryunosukeyamamoto0@gmail.com>
2021-09-04 14:27:57 +0900

    make User model and 2datas

--- groomings/models.py
+++ groomings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """ユーザーモデル"""
+    name = models.CharField(max_length=10)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    point = models.IntegerField(default=0)
