8251bad94f68b4c11b69baffb7e3e4fbd297f3d8
yamamoto.ryunosuke <ryunosukeyamamoto0@gmail.com>
2021-09-07 12:59:48 +0900

    bit modify

--- groomings/urls.py
+++ groomings/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'groomings'
 urlpatterns = [
     path(r'', views.top, name = 'top'),
-    path(r'login', views.login, name = 'login'),
-    path(r'signup', views.signup, name = 'signup'),
+    path(r'login/', views.login, name = 'login'),
+    path(r'signup/', views.signup, name = 'signup'),
     path(r'user/<int:user_id>/', views.user, name = 'user'),
-    path(r'edit/user/<int:user_id>', views.edit_user, name='edit_user')
+    path(r'edit/user/<int:user_id>/', views.edit_user, name='edit_user'),
 ]
--- groomings/views.py
+++ groomings/views.py
@@ -17,11 +17,9 @@ def signup(request):
 def user(request, user_id):
     """ユーザーページ"""
     user = User.objects.get(pk=user_id)
-    data = {"name": user.name, "point": user.point}
-    return render(request, 'groomings/user.html', data)
+    return render(request, 'groomings/user.html', context={"user": user})
 
 def edit_user(request, user_id):
     """ユーザー情報編集ページ"""
     user = User.objects.get(pk=user_id)
-    data = {"user": user}
-    return render(request, 'groomings/edit_user.html', data)
+    return render(request, 'groomings/edit_user.html', context={"user": user})
