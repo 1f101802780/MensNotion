6270fc80efed0609651946ca577c99c8e7c113dc
Ryunosuke Yamamoto <s1f101801453@iniad.org>
2021-10-12 13:22:39 +0900

    clean favopage

--- groomings/urls.py
+++ groomings/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = 'groomings'
 urlpatterns = [
-    path(r'', views.top, name = 'top'),
-    path(r'login/', views.login, name = 'login'),
-    path(r'signup/', views.signup, name = 'signup'),
-    path(r'user/<int:user_id>/', views.user, name = 'user'),
-    path(r'user/<int:user_id>/favo', views.user_favo, name='user_favo'),
-    path(r'edit/user/<int:user_id>/', views.edit_user, name='edit_user'),
+    path(r'', views.top, name = 'top'), # タイムライン
+    path(r'login/', views.login, name = 'login'), # ログイン画面
+    path(r'signup/', views.signup, name = 'signup'), # ユーザー登録画面
+    path(r'user/<int:user_id>/', views.user, name = 'user'), # ユーザー個人ページ
+    path(r'user/<int:user_id>/favo', views.user_favo, name='user_favo'), # ユーザーのお気に入り投稿ページ
+    path(r'edit/user/<int:user_id>/', views.edit_user, name='edit_user'), # ユーザー情報編集ページ
     # path(r'post',views.post, name='POST'),
-    path(r'post/', views.list_create, name='list_create'),
+    path(r'post/', views.list_create, name='list_create'), # post投稿ページ
 ]
