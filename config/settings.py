2b0851ba7aff5d0b3eb58b48641404100957647a
Ryunosuke Yamamoto <s1f101801453@iniad.org>
2022-01-06 02:35:39 +0900

    for S3

--- config/settings.py
+++ config/settings.py
@@ -161,11 +161,6 @@ LOGIN_REDIRECT_URL = '/top'
 LOGOUT_REDIRECT_URL='/login'
 
 try:
-    from .local_settings import *
-except ImportError:
-    pass
-
-try:
     from config.local_settings import *
 except ImportError:
     pass
@@ -177,23 +172,25 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     MEDIA_URL = S3_URL
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     AWS_S3_BUCKET_AUTH = False
 
-    # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     # AWS_S3_OBJECT_PARAMETERS = {
     #     'CacheControl': 'max-age=86400',
     # }
     AWS_QUERYSTRING_AUTH = False
 
-    # AWS_LOCATION = 'static'
+    AWS_LOCATION = 'static'
     
     # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+    DEFAULT_FILE_STORAGE = 'g5-t2.backends.MediaStorage'
 
 
     import django_heroku
